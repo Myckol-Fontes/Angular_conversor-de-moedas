70ca595521b4cd86c9c20bf72c7b2496fb5e87d2
Myckol Fontes <myckol.ti@gmail.com>
2022-05-23 00:16:50 -0300

    Implementando componentes na view

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ConversorComponent } from './components';
 import { ConversorService, MoedaService } from './services';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   exports: [
     ConversorComponent
