b6d8d9cdc7d8ea28aaf1571dffece2c4bc9c1827
Myckol Fontes <myckol.ti@gmail.com>
2022-05-23 00:27:51 -0300

    Criando diretivas de numero

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -4,12 +4,14 @@ import { ConversorComponent } from './components';
 import { ConversorService, MoedaService } from './services';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { NumeroDirective } from './directives';
 
 
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
