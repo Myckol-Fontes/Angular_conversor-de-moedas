ce3ddf95dc8bef3296d70b18fbc5fae0d6dede37
Myckol Fontes <myckol.ti@gmail.com>
2022-05-23 01:01:16 -0300

    Teste unitário de modais

--- src/app/conversor/utils/modal-cotacao.component.spec.ts
+++ src/app/conversor/utils/modal-cotacao.component.spec.ts
@@ -1,17 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ModalCotacaoComponent } from './modal-cotacao.component';
+import { DataBrPipe } from '../pipes';
+import { ConversorService } from '../services';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ModalCotacaoComponent', () => {
   let component: ModalCotacaoComponent;
   let fixture: ComponentFixture<ModalCotacaoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ModalCotacaoComponent ]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        ModalCotacaoComponent,
+        DataBrPipe
+      ],
+      providers: [
+        ConversorService
+      ],
+      imports: [
+        HttpClientModule
+      ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalCotacaoComponent);
